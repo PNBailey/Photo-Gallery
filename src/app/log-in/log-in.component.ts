f7fd49ae4dffc03c4fe9ec127f5baca9e9945f07
Paul Bailey <52pbailey@gmail.com>
2020-06-17 10:12:45 +0100

    Added forgotten password backend functionality

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { UsersService } from '../shared/user/users.service';
 import { LogInService } from './log-in.service';
-import { AuthResponseData } from './log-in.service'
 import { User } from '../shared/user/user.model';
 import { DataStorageService } from 'src/app/shared/data-storage.service';
 
@@ -15,33 +14,28 @@ import { DataStorageService } from 'src/app/shared/data-storage.service';
 })
 export class LogInComponent implements OnInit {
   logInForm: FormGroup;
-
-  // isLoginMode = true;
-  // forgottenPasswordMode = false;
-  // signUpMode = false;
   error: string = null;
-
   mode: string = 'logIn';
+  passwordResetEmailSent = false;
 
   constructor(
-    private router: Router, 
-    private usersService: UsersService, 
     private logInService: LogInService, 
     private dataStorageService: DataStorageService
     ) { }
 
     switchMode(chosenMode: string) {
       this.mode = chosenMode;
-      console.log(this.mode);
+      this.logInForm = this.logInService.getFormGroup(chosenMode);
+      this.error = null;
+      this.passwordResetEmailSent = false;
     }
 
   ngOnInit() {
         this.logInForm = new FormGroup({
-          'email': new FormControl(null, [Validators.required, Validators.email]),
-          'password': new FormControl(null, [Validators.required]),
-          'name': new FormControl(null),
-        })
-    }
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'password': new FormControl(null, [Validators.required])
+    })
+  }
 
   onSubmit() {
     if(!this.logInForm.valid) {
@@ -55,26 +49,36 @@ export class LogInComponent implements OnInit {
 
       const newUser = new User(email, name, likes, comments)
 
-      let authObs: Observable<AuthResponseData>;
+      let authObs: Observable<any>;
 
       if(this.mode === 'logIn') {
         authObs = this.logInService.logIn(email, password);
 
-      } else {
+      } else if(this.mode === 'signUp') {
         authObs = this.logInService.register(email, password);
+
+      } else if(this.mode === 'forgottenPassword') {
+        authObs = this.logInService.changePassword(email);
 
       }
 
-        authObs.subscribe(resData => {
-          if(this.mode !== 'logIn') {
-            this.usersService.addUser(newUser);
-            this.dataStorageService.addUsers();
-          }
+        authObs.subscribe(() => {
+
+         this.passwordResetEmailSent = this.logInService.handleSubscriptionSuccess(this.mode, newUser);
+
+          // if(this.mode === 'logIn') {
+          //   this.usersService.addUser(newUser);
+          //   this.dataStorageService.addUsers();
+          // } else if (this.mode === 'forgottenPassword') {
+          //   this.passwordResetEmailSent = true;
+          // } else {
+          //   this.router.navigate(['/gallery-list']);
+          // }
       
         this.error = null;
-        this.router.navigate(['/gallery-list'])
           
         }, errorMessage => {
+          this.passwordResetEmailSent = false;
           this.error = errorMessage;
         }); 
         this.logInForm.reset();
